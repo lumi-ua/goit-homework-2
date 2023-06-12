2f7d8fae9898b3b675f1b492888eac3bd2965a5b
lumi-ua <130182058+lumi-ua@users.noreply.github.com>
2023-06-12 17:26:28 +0300

    Next stage of fix

--- homework_1.py
+++ homework_1.py
@@ -9,26 +9,24 @@ from pathlib import Path
 
 
 ##########################################################
-img_f = ['.jpeg', '.png', '.jpg', '.svg', ".bmp"]
-mov_f = ['.avi', '.mp4', '.mov', '.mkv', ".webm", ".wmv", ".flv"]
-doc_f = ['.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx', ".ini", ".cmd", ".ppt", ".xml", ".msg", ".cpp", ".hpp"]
-mus_f = ['.mp3', '.ogg', '.wav', '.amr', ".aiff"]
-arch_f = ['.zip', '.gz', '.tar'] # shutil.ReadError: Unknown archive format gz-format
-# https://stackoverflow.com/questions/55040442/how-to-register-gz-format-in-shutil-register-archive-format-to-use-same-format
+img_f = {'.jpeg', '.png', '.jpg', '.svg', ".bmp"}
+mov_f = {'.avi', '.mp4', '.mov', '.mkv', ".webm", ".wmv", ".flv"}
+doc_f = {'.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx', ".ini", ".cmd", ".ppt", ".xml", ".msg", ".cpp", ".hpp"}
+mus_f = {'.mp3', '.ogg', '.wav', '.amr', ".aiff"}
+arch_f = {'.zip', '.tar'}
 
 ##########################################################
 CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
 TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
                "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
 
-CATEGORIES = { "images" : img_f, "video" : mov_f, "documents" : doc_f, "audio": mus_f, "archives" : arch_f }
+CATEGORIES = { "images" : img_f, "video" : mov_f, "documents" : doc_f, "audio": mus_f, "archives" : arch_f, "others" : {} }
 TRANS = {}
 for c, t in zip(CYRILLIC_SYMBOLS, TRANSLATION):
     TRANS[ord(c)] = t
     TRANS[ord(c.upper())] = t.upper()
 
 ###########################################################
-
 def getCategory(pathFile):              # (string fileName)
     for cat, exts in CATEGORIES.items():
         if pathFile.suffix.lower() in exts:
@@ -82,10 +80,10 @@ def parse_folder(root, ipath = ""):
                 pathFile.replace(targetFile)
 
             if cat == "archives":
-                shutil.unpack_archive(targetFile.absolute(), root + "/" + cat + "/" + targetFile.stem)
+                shutil.unpack_archive(str(targetFile.absolute()), root + "/" + cat + "/" + targetFile.stem)
                         
     #*********************************
-
+    # remove empty directories
     for i, dirName in enumerate(folders):
         if parse_folder(root, absPath + dirName) == True:
             # remove sub-directory if empty is OK
@@ -95,7 +93,7 @@ def parse_folder(root, ipath = ""):
     empties = True
     for iter in path.iterdir():
         if iter.is_file() or iter.is_dir():
-            # return empty-flag to allow to delete
+            # return empty-flag that allow to delete
             empties = False
             break
 
@@ -103,17 +101,44 @@ def parse_folder(root, ipath = ""):
 
 #############################################################
 
-def main():
-    try:
-        path = Path(sys.argv[1])
-    except IndexError:
-        return "No path to folder"
-    
-    if not path.exists():
-        return f"Folder with path {path} dos`n exists."
-    
-    parse_folder(sys.argv[1])
-    return "All ok"
+def printStatistic(rootStr):
+    global CATEGORIES
 
+    # try to traverse each directory-category:
+    for cat, exts in CATEGORIES.items():
+
+        dirCat = Path(rootStr + "/" + cat)
+
+        if dirCat.exists():
+            ext = set()     # known extentions
+            uext = set()    # unknown extentions
+            for item in dirCat.iterdir():
+                if(item.is_file()):
+                    print("[{}]: {}".format(cat, item.name))
+                    if item.suffix in CATEGORIES[cat]:
+                        ext.add(item.suffix)
+                    else:
+                        uext.add(item.suffix)
+            
+            # category [others] has empty extentions dictionary
+            if len(CATEGORIES[cat]) > 0:    # check, if category is any category, except [others]
+                print("[*{}*].extentions:".format(cat), ext)
+            else:                           # [others] category
+                print("[*{}*].extentions:".format(cat), uext)
+    return
+###############################################################
+def main():
+    root = sys.argv[1]
+    path = Path(root)
+
+    if not path.exists():
+        return f"Folder with path {root} dos`n exists."
+
+    parse_folder(root)
+
+    printStatistic(root)
+
+    return "All Ok"
+###############################################################
 if __name__ == "__main__":
     print(main())
